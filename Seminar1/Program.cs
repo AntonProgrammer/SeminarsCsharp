a07d216def11d365c4ebe7b3c2c0ab4664ccabd2
KincelAnton <sandbon5@yandex.ru>
2022-03-01 00:12:20 +0300

    Довавлены исходные файлы и папки

---
+++ Seminar1/Program.cs
@@ -0,0 +1,54 @@
+﻿//Задача 1: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+/*
+Console.WriteLine("Введите первое число для проверки какое больше");
+int ChisloPervoe = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число для проверки какое больше");
+int ChisloVtoroe = Convert.ToInt32(Console.ReadLine());
+
+if(ChisloPervoe > ChisloVtoroe)
+{
+    Console.WriteLine("Первое число больше");
+}
+else if(ChisloPervoe == ChisloVtoroe)
+    {
+        Console.WriteLine("Вы ввели одинаковые числа");
+    }
+    else
+    {
+        Console.WriteLine("Второе число больше");
+    }
+*/
+//===========================================================================================================
+// Задача 2: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+/*
+Console.WriteLine("Введите первое число для проверки какое больше");
+int ChisloPervoe = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число для проверки какое больше");
+int ChisloVtoroe = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите третье число для проверки какое больше");
+int ChisloTretee = Convert.ToInt32(Console.ReadLine());
+
+if (ChisloPervoe==ChisloVtoroe && ChisloVtoroe==ChisloTretee)
+{
+    Console.WriteLine("Введены одинаковые числа");
+}
+else if (ChisloPervoe > ChisloVtoroe)
+    {
+        if (ChisloPervoe > ChisloTretee)
+        {
+            Console.WriteLine("Первое число максимальное");
+        }
+    }   else if (ChisloVtoroe > ChisloTretee)
+            {
+                Console.WriteLine("Второе число максимальное");
+            }
+    
+            else
+            {
+                Console.WriteLine("Третье число максимальное");
+            }
+*/
+//=================================================================================================
