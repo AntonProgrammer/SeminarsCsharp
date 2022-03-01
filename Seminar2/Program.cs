ba37c6512d9438c0023fde741e846091b3bd3e17
KincelAnton <sandbon5@yandex.ru>
2022-03-01 19:22:08 +0300

    Добавлены решения задач ДЗ с семинара 2

---
+++ Seminar2/Program.cs
@@ -0,0 +1,117 @@
+﻿//================================================================================================================
+// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+/*
+void MetodVivodaVtoroiCifriTrehznachnogoChisla (int Chislo)
+{
+    if(Chislo>=100 && Chislo<=999)
+    {
+        int OstatokDelenie = (Chislo % 100);
+        int VtorayaCifra = (OstatokDelenie / 10);
+
+        Console.WriteLine($"Вторя цифра трёхзначного числа равна {VtorayaCifra}");
+    }
+    else
+    {
+        Console.WriteLine("Вы ввели не трёхзначное число");
+    }
+}
+
+Console.WriteLine("Введител трёхзначное число");
+int Chislo = Convert.ToInt32(Console.ReadLine());
+
+MetodVivodaVtoroiCifriTrehznachnogoChisla(Chislo);
+*/
+//===============================================================================================================
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+/*
+int MetodPodschetaKolichestvaCifrChisla(int Chislo)
+{
+    int KolichestvoCifr = 0;
+
+    while(Chislo > 0)
+    {
+        Chislo = Chislo / 10;
+        KolichestvoCifr = KolichestvoCifr + 1;
+    }
+
+    return KolichestvoCifr;
+}
+
+Console.WriteLine("Введите число");
+int Chislo = Convert.ToInt32(Console.ReadLine());
+int KolichestvoCifr = MetodPodschetaKolichestvaCifrChisla(Chislo);
+int TretiyaCifra = 0;
+
+if(KolichestvoCifr == 3)
+{
+    TretiyaCifra = Chislo % 10;
+
+    Console.WriteLine($"Третья цифра введённого числа {TretiyaCifra}");
+}
+else if(KolichestvoCifr > 3)
+    {
+        while(KolichestvoCifr > 3)
+        {
+            Chislo = Chislo / 10;
+            KolichestvoCifr = KolichestvoCifr - 1;
+        }
+
+        TretiyaCifra = Chislo % 10;
+
+        Console.WriteLine($"Третья цифра введённого числа {TretiyaCifra}");
+    }
+    else
+    {
+        Console.WriteLine("Третьей цифры нет у введённого числа");
+    }
+*/
+//==========================================================================================================
+// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+/*
+Console.WriteLine("Введите номер дня недели");
+int NomerNedeli = Convert.ToInt32(Console.ReadLine());
+
+switch(NomerNedeli)
+{
+    case 1:
+    {
+        Console.WriteLine("Нет... понедельник не выходной");
+        break;
+    }
+    case 2:
+    {
+        Console.WriteLine("Нет... вторник не выходной");
+        break;
+    }
+    case 3:
+    {
+        Console.WriteLine("Нет... среда не выходной");
+        break;
+    }
+    case 4:
+    {
+        Console.WriteLine("Нет... четверг не выходной");
+        break;
+    }
+    case 5:
+    {
+        Console.WriteLine("Нет... пятница не выходной");
+        break;
+    }
+    case 6:
+    {
+        Console.WriteLine("Да... суббота выходной");
+        break;
+    }
+    case 7:
+    {
+        Console.WriteLine("Да... воскресенье выходной");
+        break;
+    }
+    default:
+    {
+        Console.WriteLine("Такого номера дня недели нет");
+        break;
+    }
+}
+*/
