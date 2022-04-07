931c260c2261eb89b6ad187be5f1bfba5113786b
KincelAnton <sandbon5@yandex.ru>
2022-04-07 15:27:05 +0300

    ДЗ, Семинар 7, Добавлены изменения

--- Seminar7/Program.cs
+++ Seminar7/Program.cs
@@ -25,6 +25,7 @@ MassivMnVewestvennij(chisloCtrok, chisloCtolbcov);
 */
 //==============================================================================================================================================
 // Задача 2. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Вариант 1
 /*
 void PoiskZnacheniyaElementaPoIndekcamMassiva(int ctrokiNomer, int kolonkiNomer)
 {
@@ -53,6 +54,50 @@ void PoiskZnacheniyaElementaPoIndekcamMassiva(int ctrokiNomer, int kolonkiNomer)
             }
         }
         if (ctrokiNomer > massiv.GetLength(0) - 1 || ctrokiNomer < 0 || kolonkiNomer > massiv.GetLength(1) - 1 || kolonkiNomer < 0) break;
+    }
+}
+
+Console.WriteLine("Введите номер строки элемента массива");
+int nomerCtroki = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите номер столбца элемента массива");
+int nomerCtolbca = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+PoiskZnacheniyaElementaPoIndekcamMassiva(nomerCtroki, nomerCtolbca);
+*/
+// Вариант 2===================================================================================================================================
+/*
+void PoiskZnacheniyaElementaPoIndekcamMassiva(int ctrokiNomer, int kolonkiNomer)
+{
+    Random random = new Random();
+    int[,] massiv = new int[random.Next(1,11),random.Next(1,11)];
+
+    for (int i = 0; i < massiv.GetLength(0); i++)
+    {
+        for (int j = 0; j < massiv.GetLength(1); j++)
+        {
+            massiv[i,j] = random.Next(1,10);
+            Console.Write($"[{i},{j}]{massiv[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+    
+    if (ctrokiNomer > massiv.GetLength(0) - 1 || ctrokiNomer < 0 || kolonkiNomer > massiv.GetLength(1) - 1 || kolonkiNomer < 0) Console.WriteLine($"Элемент с индексом Массив[{ctrokiNomer},{kolonkiNomer}] в массиве отсутствует");
+
+    int elementNaiden = 0;
+
+    for (int i = 0; i < massiv.GetLength(0); i++)
+    {
+        for (int j = 0; j < massiv.GetLength(1); j++)
+        {
+            if(i == ctrokiNomer && j == kolonkiNomer)
+            {
+                Console.WriteLine($"Значение элеметна с индексом Масссив[{ctrokiNomer},{kolonkiNomer}] = {massiv[i,j]}");
+                elementNaiden = 1;
+                break;
+            }
+        }
+        if (elementNaiden == 1) break;
     }
 }
 
