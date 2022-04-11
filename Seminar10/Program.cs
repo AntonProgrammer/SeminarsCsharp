42ebc56d65a6b144135b74d131fddee0a982ee7b
KincelAnton <sandbon5@yandex.ru>
2022-04-11 17:49:41 +0300

    ДЗ, Семинар 10

---
+++ Seminar10/Program.cs
@@ -0,0 +1,38 @@
+﻿//=====================================================================================================================================
+// Задача. Заданы 2 массива: info b data. В массиве info хранятся двоичные представления нескольких чисел (без разделителя). В массиве data хранится информация о количестве бит, которые занимают числа из массива info. Напишите программу, которая составит массив десятичных представлений чисел массива data с учётом информации из массива info; data = {0,1,1,1,1,0,0,0,1}, info = {2,3,3,1}.
+/*
+int[] data = { 0, 1, 1, 1, 1, 0, 0, 0, 1 };
+int[] info = { 2, 3, 3, 1 };
+int indexNachalaChisla = 0;
+int intdexKonecChesla = 1;
+int cchetchik = 0;
+indexNachalaChisla = 0;
+intdexKonecChesla = 1;
+int ctepen = 0;
+
+double[] newMassiv = new double[info.Length];
+
+while (cchetchik < info.Length)
+{
+    double decyatichnoeChislo = 0;
+    ctepen = 0;
+    for (int i = intdexKonecChesla; i >= indexNachalaChisla; i--)
+    {
+        decyatichnoeChislo = decyatichnoeChislo + (data[i] * Math.Pow(2, ctepen));
+        ctepen++;
+    }
+    newMassiv[cchetchik] = decyatichnoeChislo;
+    cchetchik++;
+    if (cchetchik == 4) break;
+    indexNachalaChisla = intdexKonecChesla + 1;
+    if (cchetchik == 1) intdexKonecChesla = 1;
+    intdexKonecChesla = intdexKonecChesla + info[cchetchik];
+
+}
+
+for (int i = 0; i < newMassiv.Length; i++)
+{
+    Console.Write($"[{newMassiv[i]}] ");
+}
+*/
+//=====================================================================================================================================
